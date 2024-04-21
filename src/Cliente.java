f6b3b6f2688fad1475bbe3c9e78fbecf96dd4b26
LeonardoLaRosa <116355251+Leonardo1190@users.noreply.github.com>
2024-04-21 18:47:51 -0300

    Clases Cliente y AccionCliente

---
+++ src/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    Cliente(){}
+    Cliente (String nombre,String apellido,String email){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+}
