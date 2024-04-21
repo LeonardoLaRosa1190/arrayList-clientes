0b6c1531d85bfeb0075ccf7ce7496830003abfdf
LeonardoLaRosa <116355251+Leonardo1190@users.noreply.github.com>
2024-04-21 20:07:25 -0300

    Nuevos cambios

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Cliente {
 
-    private int idCliente;
     private String nombre;
     private String apellido;
     private String email;
-
-    Cliente(){}
+    ArrayList<Cliente> arregloCliente = new ArrayList<>();
+    Scanner entradaArgumento = new Scanner(System.in);
+    Cliente (){};
     Cliente (String nombre,String apellido,String email){
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
     }
+    public void agregaCliente(){
+        System.out.println("Ingrese el Nombre del Cliente:");
+        this.nombre = entradaArgumento.nextLine();
+        System.out.println("Ingrese el Apellido del Cliente:");
+        this.apellido = entradaArgumento.nextLine();
+        System.out.println("Ingrese el Email del Cliente:");
+        this.email = entradaArgumento.nextLine();
+        arregloCliente.add(new Cliente (nombre,apellido,email));
+        System.out.println("Cliente ingresado con éxito!!");
+    }
+    public void consultarCliente(){
+        System.out.println("Ingrese el Apellido del cliente que desea buscar");
+        for (Cliente c: arregloCliente){
+            if {
+                System.out.println("El nombre ingresado está en el sistema");
+            }else {
+                System.out.println("El nombre ingresado no está en el sistema");
+            }
+        }
+
+    }
+    public void borrarCliente(){
+        System.out.println("Ingrese el Nombre del cliente que desea buscar");
+        this.nombre = entradaArgumento.nextLine();
+        System.out.println("Ingrese el Apellido");
+        this.apellido = entradaArgumento.nextLine();
+        System.out.println("Ingrese el Email");
+        this.email = entradaArgumento.nextLine();
+        arregloCliente.add(new Cliente (nombre,apellido,email));
+    }
+
 }
--- src/PantallaCliente.java
+++ src/PantallaCliente.java
@@ -1,2 +1,39 @@
-public class PantallaCliente {
+import java.util.Scanner;
+
+public class PantallaCliente  {
+    Cliente c = new Cliente();
+    Scanner entradaTeclado = new Scanner(System.in);
+    public void buclePrincipal(){
+
+        int accion;
+        do {
+            System.out.println("Menú:");
+            System.out.println("1. Agregar Cliente");
+            System.out.println("2. Consultar Cliente");
+            System.out.println("3. Borrar Cliente");
+            System.out.println("4. Salir");
+            System.out.print("Ingresa una opción: ");
+
+
+            accion = entradaTeclado.nextInt();
+
+            switch (accion) {
+                case 1:
+                    c.agregaCliente();
+                    break;
+                case 2:
+                    c.consultarCliente();
+                    break;
+                case 3:
+                    c.borrarCliente();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa.");
+                    break;
+                default:
+                    System.out.println("Elección inválida. Por favor, selecciona una opción válida.");
+            }
+        } while (accion != 4);
+    }
 }
+
